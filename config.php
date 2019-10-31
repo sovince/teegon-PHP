0edb4e49d19247b92f163a2632f7f9f7da100e13
Sun Wang <so_vince@outlook.com>
2019-10-31 08:28:07 +0800

    Update config.php

--- config.php
+++ config.php
@@ -3,5 +3,9 @@ define('TEE_SITE_URL', 'https://teegon.com/');
 define('TEE_API_URL', 'https://api.teegon.com/');
 define('TEE_CLIENT_ID', 'vk5pjf6hlpoefh4zgtyzb7xl');
 define('TEE_CLIENT_SECRET', 'lutiiwptjs475jiqc664q7f4xal542a3');
+//admin_username
+//%E7%A7%A6%E8%A5%BF%E5%BF%B5
 
+//vadmin_username
+//4617KccvITQn1QnwTQJkpVeac%2BesqzX4ijaySAKVxBQPhwtz8uY
 
